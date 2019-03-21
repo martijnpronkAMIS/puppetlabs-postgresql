fc3a3cc8b26398067fcd3b457b4ba533ae342ae9
Martijn Pronk <martijn.pronk@amis.nl>
2019-03-21 13:46:52 +0100

    Revert "pause_at_recovery_target is no more"
    This reverts commit 8e7b3950fedb5efbb94cfed53800f86594e0f055.

--- manifests/server/recovery.pp
+++ manifests/server/recovery.pp
@@ -9,6 +9,7 @@ define postgresql::server::recovery(
   $recovery_target_inclusive      = undef,
   $recovery_target                = undef,
   $recovery_target_timeline       = undef,
+  $pause_at_recovery_target       = undef,
   $standby_mode                   = undef,
   $primary_conninfo               = undef,
   $primary_slot_name              = undef,
@@ -23,7 +24,7 @@ define postgresql::server::recovery(
     if($restore_command == undef and $archive_cleanup_command == undef and $recovery_end_command == undef
       and $recovery_target_name == undef and $recovery_target_time == undef and $recovery_target_xid == undef
       and $recovery_target_inclusive == undef and $recovery_target == undef and $recovery_target_timeline == undef
-      and $pause_at_recovery_target == undef and $primary_conninfo == undef
+      and $pause_at_recovery_target == undef and $standby_mode == undef and $primary_conninfo == undef
       and $primary_slot_name == undef and $trigger_file == undef and $recovery_min_apply_delay == undef) {
       fail('postgresql::server::recovery use this resource but do not pass a parameter will avoid creating the recovery.conf, because it makes no sense.')
     }
